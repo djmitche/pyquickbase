54de603afbd37738946a9bfcb3d661255d7dd84f
Dustin J. Mitchell <dustin@mozilla.com>
2012-04-06 12:58:43 -0500

    add setup.py, rename module properly

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='pyquickbase',
+    version='1.0',
+    description='Python interface to the QuickBase API',
+    author='Dustin J. Mitchell',
+    author_email='dustin@cs.uchicago.edu',
+    url='http://github.com/djmitche/pyquickbase',
+    py_modules=['pyquickbase'],
+)
